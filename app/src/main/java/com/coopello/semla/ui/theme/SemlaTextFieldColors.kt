0e2d305af0661da28545d0de8c35b9a6b2520c20
Kaito Dogi <kendobu0405@gmail.com>
2022-08-15 02:21:10 +0900

    :lipstick: TextField の背景を白にした

--- app/src/main/java/com/coopello/semla/ui/theme/SemlaTextFieldColors.kt
+++ app/src/main/java/com/coopello/semla/ui/theme/SemlaTextFieldColors.kt
@@ -7,9 +7,11 @@ import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SemlaTextFieldColors(
+    backgroundColor: Color = Color.White,
     cursorColor: Color = Blue,
     focusedIndicatorColor: Color = Blue
 ) = TextFieldDefaults.textFieldColors(
+    backgroundColor = backgroundColor,
     cursorColor = cursorColor,
     focusedIndicatorColor = focusedIndicatorColor.copy(alpha = ContentAlpha.high)
 )
