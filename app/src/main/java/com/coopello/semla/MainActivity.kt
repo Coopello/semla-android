459049379a2ff892c9629a765de8d7497a81b676
Kaito Dogi <kendobu0405@gmail.com>
2022-08-14 23:00:40 +0900

    :lipstick: Topbar を追加

--- app/src/main/java/com/coopello/semla/MainActivity.kt
+++ app/src/main/java/com/coopello/semla/MainActivity.kt
@@ -6,9 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.coopello.semla.ui.theme.SemlaTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +21,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             SemlaTheme {
                 Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = stringResource(R.string.app_name),
+                                    fontWeight = FontWeight.W700
+                                )
+                            },
+                            backgroundColor = colorResource(R.color.white),
+                            contentColor = colorResource(R.color.blue)
+                        )
+                    },
                     floatingActionButton = {
                         FloatingActionButton(onClick = { /* TODO: Qiita に投稿する処理を実装 */ }) {
                             Icon(
