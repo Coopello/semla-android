5c2095ffbe38fec498da1b20154a48c1c3de5b74
Kaito Dogi <kendobu0405@gmail.com>
2022-08-14 19:39:04 +0900

    :adhesive_bandage: 自動生成されたコードに ktlint を適用

--- app/src/main/java/com/coopello/semla/MainActivity.kt
+++ app/src/main/java/com/coopello/semla/MainActivity.kt
@@ -13,28 +13,31 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.coopello.semla.ui.theme.SemlaTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      SemlaTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SemlaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Composable
 fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-  SemlaTheme {
-    Greeting("Android")
-  }
+    SemlaTheme {
+        Greeting("Android")
+    }
 }
