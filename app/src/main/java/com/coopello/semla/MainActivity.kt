0e03ae816e6d5deda076fe16d526ff3930bf0d64
Kaito Dogi <kendobu0405@gmail.com>
2022-08-15 02:08:04 +0900

    :lipstick: LazyColumn で SectionCard を表示

--- app/src/main/java/com/coopello/semla/MainActivity.kt
+++ app/src/main/java/com/coopello/semla/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.coopello.semla
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -10,12 +16,16 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.coopello.semla.ui.component.SectionCard
 import com.coopello.semla.ui.theme.SemlaTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -42,8 +52,27 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 ) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(16.dp)
+                    ) {
+                        items(
+                            items = MockData.sectionList,
+                            contentType = { it }
+                        ) {
+                            SectionCard(
+                                title = it,
+                                placeholder = it
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+private object MockData {
+    val sectionList = listOf("一言まとめ", "深掘りした内容", "自分の考え", "参考文献")
 }
