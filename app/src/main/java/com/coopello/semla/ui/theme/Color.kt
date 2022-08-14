e22724c091e33f4dc85c997f6f6ca874b3bd599d
Kaito Dogi <kendobu0405@gmail.com>
2022-08-14 23:53:58 +0900

    :lipstick: MaterialTheme に Gray を追加

--- app/src/main/java/com/coopello/semla/ui/theme/Color.kt
+++ app/src/main/java/com/coopello/semla/ui/theme/Color.kt
@@ -5,3 +5,4 @@ import androidx.compose.ui.graphics.Color
 val LightBlue = Color(0xFF91FBFF)
 val Blue = Color(0xFF5AC8D8)
 val DarkBlue = Color(0xFF1197A7)
+val Gray = Color(0xFFF5F6F6)
--- app/src/main/java/com/coopello/semla/ui/theme/Theme.kt
+++ app/src/main/java/com/coopello/semla/ui/theme/Theme.kt
@@ -2,29 +2,33 @@ package com.coopello.semla.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = LightBlue,
-    primaryVariant = DarkBlue,
-    secondary = LightBlue
-)
+// private val DarkColorPalette = darkColors(
+//    primary = LightBlue,
+//    primaryVariant = DarkBlue,
+//    secondary = LightBlue,
+//    secondaryVariant = DarkBlue
+// )
 
 private val LightColorPalette = lightColors(
     primary = Blue,
     primaryVariant = DarkBlue,
-    secondary = Blue
+    secondary = Blue,
+    secondaryVariant = DarkBlue,
+    background = Gray
 )
 
 @Composable
 fun SemlaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
