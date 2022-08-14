82a00c23f3b5e8b7593d82e1c8d8207b7efde709
Kaito Dogi <kendobu0405@gmail.com>
2022-08-14 19:37:24 +0900

    :lipstick: テーマカラーを追加

--- app/src/main/java/com/coopello/semla/ui/theme/Color.kt
+++ app/src/main/java/com/coopello/semla/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.coopello.semla.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightBlue = Color(0xFF91FBFF)
+val Blue = Color(0xFF5AC8D8)
+val DarkBlue = Color(0xFF1197A7)
+
+// 暫定的に Secondary Color を配置
+val Red = Color(0xFFF05454)
